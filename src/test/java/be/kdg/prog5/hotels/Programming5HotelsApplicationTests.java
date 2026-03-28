268dfeb539bbfc67cfa504f55ae32cebfef67cda
Tanmoy Das <tanmoy.das@student.kdg.be>
2026-03-28 04:07:14 +0100

    week6: configure contextLoads test with @ActiveProfiles("test")

--- src/test/java/be/kdg/prog5/hotels/Programming5HotelsApplicationTests.java
+++ src/test/java/be/kdg/prog5/hotels/Programming5HotelsApplicationTests.java
@@ -1,13 +1,13 @@
 package be.kdg.prog5.hotels;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")    // use application-test.properties instead of the normal configuration.
 class Programming5HotelsApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
