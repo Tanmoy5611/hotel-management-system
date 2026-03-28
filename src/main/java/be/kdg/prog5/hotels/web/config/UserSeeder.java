b5b6e2651ca3ac19d27ede1a768163e317d98aa5
Tanmoy Das <tanmoy.das@student.kdg.be>
2026-03-28 04:10:07 +0100

    week6: adjusted user seeding to support test isolation and profiles

--- src/main/java/be/kdg/prog5/hotels/web/config/UserSeeder.java
+++ src/main/java/be/kdg/prog5/hotels/web/config/UserSeeder.java
@@ -8,12 +8,14 @@ import be.kdg.prog5.hotels.domain.VIPGuest;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Configuration
+@Profile("!test") //  prevent test data from interfering with production
 public class UserSeeder {
 
     // runs automatically when the application starts
